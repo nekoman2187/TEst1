5a9eb09809675dcdefa00be00edaba63d1387423
watanabe akinori <nekoman@mac.com>
2022-10-19 21:08:30 +0900

    test

---
+++ ImportTest2/src/ImportTest2.c
@@ -0,0 +1,18 @@
+/***********************************************************************
+*
+*  FILE        : ImportTest2.c
+*  DATE        : 2022-05-17
+*  DESCRIPTION : Main Program
+*
+*  NOTE:THIS IS A TYPICAL EXAMPLE.
+*
+***********************************************************************/
+#include "r_smc_entry.h"
+
+void main(void);
+extern int tata();
+
+void main(void)
+{
+	tata();
+}
---
+++ ImportTest2/src/test.c
@@ -0,0 +1,12 @@
+/*
+ * test.c
+ *
+ *  Created on: 2022/05/17
+ *      Author: nekom
+ */
+
+
+int tata();
+int tata() {
+	return 0;
+}
